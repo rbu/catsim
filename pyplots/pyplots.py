1bd1f5cdbf3cae20381f5c505c2608d3a092b99c
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 11:52:54 -0200

    better documentation

--- pyplots/pyplots.py
+++
@@ -1,7 +0,0 @@
-def plot_item_curve():
-    from catsim.cat import generate_item_bank
-    from catsim import plot
-    item = generate_item_bank(1)[0]
-    plot.item_curve(item[0], item[1], item[2], ptype='icc')
-    plot.item_curve(item[0], item[1], item[2], ptype='iic')
-    plot.item_curve(item[0], item[1], item[2], ptype='both')
